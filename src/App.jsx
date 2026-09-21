3d51767b8c0e22932167c05f5d99d9d64b922e0b
AkshayValtanitan <shanenathanarchival@gmail.com>
2026-09-21 18:01:10 +0800

    1.4: course information step 4

--- src/App.jsx
+++ src/App.jsx
@@ -42,34 +42,37 @@ const Content = (props) => {
 console.log(props)
   return (
     <div className="">
-        <Parts subject={props.subject1} units={props.units1}/>
-        <Parts subject={props.subject2} units={props.units2}/>
-        <Parts subject={props.subject3} units={props.units3}/>
+        <Parts subject={props.subject[0].name} units={props.subject[0].unitCount}/>
+        <Parts subject={props.subject[1].name} units={props.subject[1].unitCount}/>
+        <Parts subject={props.subject[2].name} units={props.subject[2].unitCount}/>
     </div>
   )
 }
 const App = () => {
   const course = 'Information Technology'
-  const subject1 = {
-    name: 'CSIT340-Industry Elective 1',
-    unitCount: 3
-  }
-  const subject2 = {
-    name: 'FL033-Nihongo 1',
-    unitCount: 3
-  }
-  const subject3 = {
-    name: 'CSIT342-Industry Elective 3',
-    unitCount: 3
-  }
+  const subjects = [
+    {
+      name: 'CSIT340-Industry Elective 1',
+      unitCount: 3
+    },
+    {
+      name: 'FL033-Nihongo 1',
+      unitCount: 3
+    },
+    {
+      name: 'CSIT342-Industry Elective 3',
+      unitCount: 3
+    }
+  ]
 
   return (
     <>
       <Header course={course}/>
-      <Content subject1={subject1.name} units1={subject1.unitCount}
-               subject2={subject2.name} units2={subject2.unitCount}
-               subject3={subject3.name} units3={subject3.unitCount}/>
-      <Total units1={subject1.unitCount} units2={subject2.unitCount} units3={subject3.unitCount}/>
+      {/* <Content subject1={subjects[0].name} units1={subjects[0].unitCount}
+               subject2={subjects[1].name} units2={subjects[1].unitCount}
+               subject3={subjects[2].name} units3={subjects[2].unitCount}/> */}
+      <Content subject = {subjects}/>
+      <Total units1={subjects[0].unitCount} units2={subjects[1].unitCount} units3={subjects[2].unitCount}/>
     </>
   )
 }
